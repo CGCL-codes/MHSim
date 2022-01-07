e43daad3c7dd1511a540245d622f0e8b49c7fb34
jhxu <45053998+jhxu001@users.noreply.github.com>
2022-01-07 20:52:59 +0800

    Delete NeuroSim/.exvim.n directory

--- NeuroSim/.exvim.n/update-idutils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.n"
-export TOOLS="/home/xujh/exvim/vimfiles/tools/"
-export TMP="${DEST}/_ID"
-export TARGET="${DEST}/ID"
-sh ${TOOLS}/shell/bash/update-idutils.sh
--- NeuroSim/.exvim.n/update-inherits.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.n"
-export TOOLS="/home/xujh/exvim/vimfiles/tools/"
-export TMP="${DEST}/_inherits"
-export TARGET="${DEST}/inherits"
-sh ${TOOLS}/shell/bash/update-inherits.sh
