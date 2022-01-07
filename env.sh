f471068fb2547ad3838ecec5b3caed677ba9ef6c
jhxu001 <565902301@qq.com>
2022-01-07 22:28:40 +0800

    modified:   env.sh

--- env.sh
+++ env.sh
@@ -3,7 +3,8 @@ NEUROSIMPATH=$MHSim/NeuroSim/ #root path of NeuroSim
 PINPATH=$MHSim/zsim/pin-2.14/  #path of pin_tool
 LIBCONFIG= #path of libconfig
 HDF5= #path of hdf5
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF5/lib:$LIBCONFIG:/lib
+LIBELF= #path of libelf
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF5/lib:$LIBCONFIG:$LIBELF/lib:/lib
 INCLUDE=$INCLUDE:$HDF5/include:$LIBCONFIG:/include
 LIBRARY_PATH=$LIBRARY_PATH:$HDF5/lib
 export PINPATH NEUROSIMPATH HDF5 LD_LIBRARY_PATH LIBRARY_PATH
